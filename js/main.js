0e71a949bd9a354314965143030729c9a25c7a3c
edwinalegre <edwin_alegre@yahoo.com>
2014-12-18 17:40:44 -0800

    Added css properties and game pieces [Finishes #84474780]

--- js/main.js
+++ js/main.js
@@ -16,3 +16,30 @@ $scope.start = function(){
         $scope.status = 'Player X Starts';
     };
 };
+
+$scope.onclick = function(){
+if(this.row.box == '' && game == ''){
+    if(turn == 'X'){
+        this.row.box = 'X';
+        turn = 'O';
+    }else{
+        this.row.box = 'O';
+        turn = 'X';
+    };
+
+    // Stops current game and displays game status
+    $scope.score = function(i){
+        if (i == 'X'){
+            p1scoreWin++;
+            $scope.p1scoreWin = p1scoreWin;
+        }else{
+            p1scoreLose++;
+            $scope.p1scoreLose = p1scoreLose;
+        }
+        $scope.startButton = 'Play Again';
+        $scope.status = i + ' is the winner!!!';
+        game = 'stop';
+        turn = i;
+    };
+
+};
