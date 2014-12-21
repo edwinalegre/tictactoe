a50a6628e95bb2aa1904e985af9a84fd3eaa805a
edwinalegre <edwin_alegre@yahoo.com>
2014-12-20 23:26:07 -0800

    Updated site with connection to database in Firebase [Finishes #84896314]

--- js/TicController.js
+++ js/TicController.js
@@ -8,32 +8,51 @@ angular
 
         // Inject Functions
         theController = this;
-        this.boxes = getTic();
+        this.boxesList = getTic();
         this.start = start;
         this.onclick = onclick;
+        // var ticObj = $firebase(new Firebase('https://haon.firebaseio.com/tic/squares')).$asObject();
+        var ticObj = $firebase(new Firebase('https://haon.firebaseio.com/tic/game')).$asObject();
+
+        console.log(ticObj.$id[0]);
 
         function getTic() {
-            var ref = new Firebase('https://haon.firebaseio.com/tic');
+            var ref = new Firebase('https://haon.firebaseio.com/tic/squares');
             var tic = $firebase(ref).$asObject();
             return tic;
         };
 
+        // test();
+        // function test(){
+        //     //ticObj
+        //     theController.boxesList.squares = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+        //     theController.boxesList.players = [{name: 'fred', wins: 0, loses: 1},{name: 'ed', wins: 2, loses: 3}];
+        //     theController.boxesList.$save();
+        // }
+
         // Sets initial variables
         var turn = 'O';
-        var game = '';
+        var game = 'stop';
         var p1scoreWin = 0;
         var p1scoreLose = 0;
         var p1scoreTie = 0;
         this.startButton = 'Start';
 
         // Start and Restart game
-        function start(){
-            game = '';
-            this.boxes = [
-                    {box: ''},{box: ''},{box: ''},
-                    {box: ''},{box: ''},{box: ''},
-                    {box: ''},{box: ''},{box: ''}
-                ]; 
+        function start(i){
+
+            this.boxesList[0] = ' ';
+            this.boxesList[1] = ' ';
+            this.boxesList[2] = ' ';
+            this.boxesList[3] = ' ';
+            this.boxesList[4] = ' ';
+            this.boxesList[5] = ' ';
+            this.boxesList[6] = ' ';
+            this.boxesList[7] = ' ';
+            this.boxesList[8] = ' ';
+            this.boxesList.$save();
+
+            game = 'start';
             if(turn == 'X'){
                 turn = 'O';
                 this.status = 'Player O Starts';
@@ -45,13 +64,16 @@ angular
 
         // Players choices
         function onclick(i){
-            console.log(i);
-            if(this.boxes[i].box == '' && game == ''){
+
+            if(this.boxesList[i] == ' ' && game == 'start'){
+                console.log('hi')
                 if(turn == 'X'){
-                    this.boxes[i].box = 'X';
+                    this.boxesList[i] = 'X';
+                    this.boxesList.$save();
                     turn = 'O';
                 }else{
-                    this.boxes[i].box = 'O';
+                    this.boxesList[i] = 'O';
+                    this.boxesList.$save();
                     turn = 'X';
                 };
 
@@ -71,69 +93,68 @@ angular
                 };
 
                 // Game comparison of X's and O's on the board to determine the winner and losers
-                if ( theController.boxes[0].box == 'X' && theController.boxes[1].box == 'X' && theController.boxes[2].box == 'X' ){
+                if ( this.boxesList[0] == 'X' && this.boxesList[1] == 'X' && this.boxesList[2] == 'X' ){
+                    score('X');
+                    console.log('hi')
+                };
+                if ( this.boxesList[3] == 'X' && this.boxesList[4] == 'X' && this.boxesList[5] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[3].box == 'X' && theController.boxes[4].box == 'X' && theController.boxes[5].box == 'X' ){
+                if ( this.boxesList[6] == 'X' && this.boxesList[7] == 'X' && this.boxesList[8] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[6].box == 'X' && theController.boxes[7].box == 'X' && theController.boxes[8].box == 'X' ){
+                if ( this.boxesList[0] == 'X' && this.boxesList[3] == 'X' && this.boxesList[6] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[0].box == 'X' && theController.boxes[3].box == 'X' && theController.boxes[6].box == 'X' ){
+                if ( this.boxesList[1] == 'X' && this.boxesList[4] == 'X' && this.boxesList[7] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[1].box == 'X' && theController.boxes[4].box == 'X' && theController.boxes[7].box == 'X' ){
+                if ( this.boxesList[2] == 'X' && this.boxesList[5] == 'X' && this.boxesList[8] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[2].box == 'X' && theController.boxes[5].box == 'X' && theController.boxes[8].box == 'X' ){
+                if ( this.boxesList[0] == 'X' && this.boxesList[4] == 'X' && this.boxesList[8] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[0].box == 'X' && theController.boxes[4].box == 'X' && theController.boxes[8].box == 'X' ){
+                if ( this.boxesList[2] == 'X' && this.boxesList[4] == 'X' && this.boxesList[6] == 'X' ){
                     score('X');
                 };
-                if ( theController.boxes[2].box == 'X' && theController.boxes[4].box == 'X' && theController.boxes[6].box == 'X' ){
-                    score('X');
-                };
-                if ( theController.boxes[0].box == 'O' && theController.boxes[1].box == 'O' && theController.boxes[2].box == 'O' ){
+                if ( this.boxesList[0] == 'O' && this.boxesList[1] == 'O' && this.boxesList[2] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[3].box == 'O' && theController.boxes[4].box == 'O' && theController.boxes[5].box == 'O' ){
+                if ( this.boxesList[3] == 'O' && this.boxesList[4] == 'O' && this.boxesList[5] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[6].box == 'O' && theController.boxes[7].box == 'O' && theController.boxes[8].box == 'O' ){
+                if ( this.boxesList[6] == 'O' && this.boxesList[7] == 'O' && this.boxesList[8] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[0].box == 'O' && theController.boxes[3].box == 'O' && theController.boxes[6].box == 'O' ){
+                if ( this.boxesList[0] == 'O' && this.boxesList[3] == 'O' && this.boxesList[6] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[1].box == 'O' && theController.boxes[4].box == 'O' && theController.boxes[7].box == 'O' ){
+                if ( this.boxesList[1] == 'O' && this.boxesList[4] == 'O' && this.boxesList[7] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[2].box == 'O' && theController.boxes[5].box == 'O' && theController.boxes[8].box == 'O' ){
+                if ( this.boxesList[2] == 'O' && this.boxesList[5] == 'O' && this.boxesList[8] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[0].box == 'O' && theController.boxes[4].box == 'O' && theController.boxes[8].box == 'O' ){
+                if ( this.boxesList[0] == 'O' && this.boxesList[4] == 'O' && this.boxesList[8] == 'O' ){
                     score('O');
                 };
-                if ( theController.boxes[2].box == 'O' && theController.boxes[4].box == 'O' && theController.boxes[6].box == 'O' ){
+                if ( this.boxesList[2] == 'O' && this.boxesList[4] == 'O' && this.boxesList[6] == 'O' ){
                     score('O');
                 };
 
                 // Tie game
-                if ( theController.boxes[0].box != '' && theController.boxes[1].box != '' && theController.boxes[2].box != '' &&
-                     theController.boxes[3].box != '' && theController.boxes[4].box != '' && theController.boxes[5].box != '' &&
-                     theController.boxes[6].box != '' && theController.boxes[7].box != '' && theController.boxes[8].box != '' && game != 'stop'){
+                if ( this.boxesList[0] != ' ' && this.boxesList[1] != ' ' && this.boxesList[2] != ' ' &&
+                     this.boxesList[3] != ' ' && this.boxesList[4] != ' ' && this.boxesList[5] != ' ' &&
+                     this.boxesList[6] != ' ' && this.boxesList[7] != ' ' && this.boxesList[8] != ' ' && game != 'stop'){
                     
-                    theController.startButton = 'Play Again';
-                    theController.status = 'TIE!!!';
+                    this.startButton = 'Play Again';
+                    this.status = 'TIE!!!';
                     p1scoreTie++;
-                    theController.p1scoreTie = p1scoreTie;
+                    this.p1scoreTie = p1scoreTie;
                     game = 'stop';
                     turn == 'X' ? turn = 'O' : turn = 'X';
                 };
-
             };
         };
-
     };
